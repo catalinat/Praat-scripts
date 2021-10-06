6d04ed5684f4b5967246aa4facfbb4c432466c11
Catalina Torres <31749997+catalinat@users.noreply.github.com>
2021-10-06 09:47:34 +0200

    changed path

--- cleanup_textgrids.R
+++ cleanup_textgrids.R
@@ -6,22 +6,22 @@
 #	Phonetics laboratory (The University of Melbourne)
 
 
-remove_text_from_tier_2 <- function(source_file){
+remove_text_from_tier_5 <- function(source_file){
   
   text_grid <-  readLines(source_file)
   
   new_text_grid = c()
   
-  in_tier_2 <- FALSE
+  in_tier_5 <- FALSE
   for (row in text_grid){
-    if(grepl("item [2]:", row, fixed=TRUE)){
-      in_tier_2 <- TRUE
+    if(grepl("item [5]:", row, fixed=TRUE)){
+      in_tier_5 <- TRUE
     }
-    if(grepl("item [3]:", row, fixed=TRUE)){
-      in_tier_2 <- FALSE
+    if(grepl("item [6]:", row, fixed=TRUE)){
+      in_tier_5 <- FALSE
     }
     
-    if(in_tier_2){
+    if(in_tier_5){
       row <- gsub("text = \".*\"", "text = \"\"", row)
     }
     
@@ -41,10 +41,9 @@ remove_text_from_tier_2 <- function(source_file){
 # Find multiple files in directory
 fix_textgrids_in_directory <- function(directory){
   for (f in list.files(directory, full.names = TRUE, pattern = ".+TextGrid")) {
-    remove_text_from_tier_2(f)
+    remove_text_from_tier_5(f)
   }
 }
 
 # Select directory to be used
-fix_textgrids_in_directory("/Users/catalina/Desktop/TextGrids")
-#fix_textgrids_in_directory("/Users/catalinat/Desktop/TEST-MAUS/regex-test")
+fix_textgrids_in_directory("/Users/catalina/Desktop/Pitjantjara_ConverbProsody/data-30.08.2021/Ana3-batches/Batch 20")
